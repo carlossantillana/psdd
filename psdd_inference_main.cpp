b2bf6d04c5032ca5b069651cf0bc09da9fd64768
Carlos Santillana <csantillana21@gmail.com>
2020-03-26 04:46:06 +0000

    added most code for kernel, just need to pass proper params.. and see what else from there

--- psdd_inference_main.cpp
+++ psdd_inference_main.cpp
@@ -8,8 +8,14 @@
 #include <psdd/optionparser.h>
 #include <psdd/fpga_psdd_node.h>
 #include <psdd/fpga_evaluate.h>
+#include <string>
 #include <fstream>
 #include <xcl2/xcl2.hpp>
+#include <cmath>
+#include <vector>
+#include <string>
+using std::string;
+using std::vector;
 
 extern "C" {
 #include <sdd/sddapi.h>
@@ -101,7 +107,6 @@ int main(int argc, const char *argv[]) {
   std::bitset<MAX_VAR> instantiation;
   std::ifstream File;
   File.open("allPossibleSolutions.txt");
-  // int REVERT_LATER = 0;
   for(int a = 0; a < 55; a++){
     int tmp;
     File >> tmp;
@@ -116,8 +121,71 @@ int main(int argc, const char *argv[]) {
 
   //FPGA
   float result [NUM_QUERIES] = {0};
-  EvaluateWithoutPointer(var_mask, instantiation, fpga_serialized_psdd_,
-    fpga_node_vector, children_vector, parameter_vector, bool_param_vector, result, flippers);
+  //init Sizes
+  size_t var_mask_size_bytes = sizeof(var_mask);
+  size_t instantiation_size_bytes = sizeof(instantiation);
+  size_t fpga_serialized_psdd_size_bytes = sizeof(fpga_serialized_psdd_[0]) * PSDD_SIZE;
+  size_t fpga_node_vector_size_bytes = sizeof(fpga_node_vector[0]) * PSDD_SIZE;
+  size_t children_vector_size_bytes = sizeof(children_vector[0]) * TOTAL_CHILDREN;
+  size_t parameter_vector_size_bytes = sizeof(parameter_vector[0]) * 770511;
+  size_t bool_param_vector_size_bytes = sizeof(bool_param_vector[0]) * 792;
+  size_t flippers_size_bytes = sizeof(flippers[0]) * 55;
+  size_t result_size_bytes = sizeof(result[0]) * NUM_QUERIES;
+  // EvaluateWithoutPointer(var_mask, instantiation, fpga_serialized_psdd_,
+  //   fpga_node_vector, children_vector, parameter_vector, bool_param_vector, result, flippers);
+    vector<cl::Device> devices = xcl::get_xil_devices();
+    cl::Device device = devices[0];
+
+    cl::Context context(device);
+    cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
+    string device_name = device.getInfo<CL_DEVICE_NAME>();
+
+    string binaryFile = xcl::find_binary_file(device_name, "src/fpga_evaluate");
+    cl::Program::Binaries bins = xcl::import_binary_file(binaryFile);
+    devices.resize(1);
+    cl::Program program(context, devices, bins);
+    cl::Kernel kernel(program, "fpga_evaluate");
+
+    //Allocate Buffer in Global Memory
+    vector<cl::Memory> inBufVec, outBufVec;
+    cl::Buffer buffer_in1(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                          var_mask_size_bytes, &var_mask);
+    cl::Buffer buffer_in2(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                          instantiation_size_bytes, &instantiation);
+    cl::Buffer buffer_in3(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                          fpga_serialized_psdd_size_bytes, &fpga_serialized_psdd);
+    cl::Buffer buffer_in4(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                          fpga_node_vector_size_bytes, &fpga_node_vector);
+    cl::Buffer buffer_in5(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                          children_vector_size_bytes, &children_vector);
+    cl::Buffer buffer_in6(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                          parameter_vector_size_bytes, &parameter_vector);
+    cl::Buffer buffer_in7(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                          bool_param_vector_size_bytes, &bool_param_vector);
+    cl::Buffer buffer_in8(context,CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                          flippers_size_bytes, &flippers);
+    cl::Buffer buffer_output(context,CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+                             result_size_bytes, &result);
+    inBufVec.push_back(buffer_in1);
+    inBufVec.push_back(buffer_in2);
+    outBufVec.push_back(buffer_output);
+
+    //Copy input data to device global memory
+    q.enqueueMigrateMemObjects(inBufVec, 0/* 0 means from host*/);
+
+    auto krnl_vector_add = cl::KernelFunctor<cl::Buffer&, cl::Buffer&,
+                                             cl::Buffer&, int>(kernel);
+
+    //Launch the Kernel
+    //fix input to this kernel
+    krnl_vector_add(cl::EnqueueArgs(q, cl::NDRange(1, 1, 1), cl::NDRange(1, 1, 1)),
+                    buffer_in1, buffer_in2, buffer_output, 55);
+
+    //Copy Result from Device Global Memory to Host Local Memory
+    q.enqueueMigrateMemObjects(outBufVec, CL_MIGRATE_MEM_OBJECT_HOST);
+    q.finish();
+
+
     bool validResults = verifyResults(result, psdd_filename, reference_psdd_manager, var_mask, instantiation, flippers);
     delete (psdd_manager);
     delete (reference_psdd_manager);
